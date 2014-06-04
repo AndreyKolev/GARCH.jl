46659e1dc66b6379f29f0a187347e708f1e2d741
AndreyKolev <kolevraw@gmail.com>
2014-06-04 22:42:51 +0400

    fixed test and warnings

--- src/tests.jl
+++ src/tests.jl
@@ -2,9 +2,9 @@
 function jbtest(x::Vector)
   n = length(x)
   m1 = sum(x)/n
-  m2 = sum((x - m1).^2)/n
-  m3 = sum((x - m1).^3)/n
-  m4 = sum((x - m1).^4)/n
+  m2 = sum((x .- m1).^2)/n
+  m3 = sum((x .- m1).^3)/n
+  m4 = sum((x .- m1).^4)/n
   b1 = (m3/m2^(3/2))^2
   b2 = (m4/m2^2)
   statistic = n * b1/6 + n*(b2 - 3)^2/24
--- test/GARCHtest.jl
+++ test/GARCHtest.jl
@@ -2,11 +2,11 @@ using Base.Test
 using GARCH
 
 filename = Pkg.dir("GARCH", "test","data","SPY.csv")
-close = readcsv(filename,Float64)[:,2]
+close = float(readcsv(filename)[:,2])
 ret = diff(log(close))
-ret = ret - mean(ret)
+ret = ret .- mean(ret)
 fit = garchFit(ret)
 param = [2.469347e-06, 1.142268e-01, 8.691734e-01] #R fGarch garch(1,1) estimated params
-@test_approx_eq_eps(fit.params,param,1e-3)
+@test_approx_eq_eps(fit.params,param, 1e-3)
 
 
