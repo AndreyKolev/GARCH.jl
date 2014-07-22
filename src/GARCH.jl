278d15c1e8dd1f4ff95de9e8c2c4967bd16a98ef
AndreyKolev <kolevraw@gmail.com>
2014-07-23 00:34:15 +0400

    Changed a REQUIRE file and file naming fixes

--- src/GARCH.jl
+++ src/GARCH.jl
@@ -8,7 +8,7 @@ using NLopt, Distributions
 
 export garchFit, garchPkgTest, predict
 
-include("tests.jl")
+include("stattests.jl")
 
 type GarchFit
   data::Vector
@@ -24,8 +24,8 @@ type GarchFit
 end
 
 function Base.show(io::IO ,fit::GarchFit)
-  pnorm(x) = 0.5*(1+erf(x/sqrt(2)))
-  prt(x) = 2*(1-pnorm(abs(x)))
+  pnorm(x) = 0.5 * (1 + erf(x / sqrt(2)))
+  prt(x) = 2 * (1 - pnorm(abs(x)))
   @printf io "Fitted garch model \n"
   @printf io " * Coefficient(s): \tomega \t\talpha \t\tbeta\n"
   @printf io "   \t\t\t%f\t%f\t%f\n" fit.params[1] fit.params[2] fit.params[3]
@@ -34,7 +34,7 @@ function Base.show(io::IO ,fit::GarchFit)
   @printf io " * Solver status: %s\n\n" fit.status
   println(io," * Standardised Residuals Tests:")
   println(io,"   \t\t\t\tStatistic\tp-Value")
-  jbstat,jbp = jbtest(fit.data./fit.sigma);
+  jbstat, jbp = jbtest(fit.data./fit.sigma);
   @printf io "   Jarque-Bera Test\t\U1D6D8\u00B2\t%.6f\t%.6f\n\n" jbstat jbp
   println(io," * Error Analysis:")
   println(io,"   \t\tEstimate\t\Std.Error\tt value \tPr(>|t|)")
@@ -43,7 +43,7 @@ function Base.show(io::IO ,fit::GarchFit)
   @printf io "   beta \t%f\t%f\t%f\t%f\n"  fit.params[3] fit.secoef[3] fit.tval[3] prt(fit.tval[3])
 end
 
-function cdHessian(par,LLH)
+function cdHessian(par, LLH)
   eps = 1e-4 * par
   n = length(par)
   H = zeros(n,n)
@@ -67,7 +67,7 @@ function cdHessian(par,LLH)
   H
 end
 
-function garchLLH(rets::Vector,x::Vector)
+function garchLLH(rets::Vector, x::Vector)
   rets2   = rets.^2;
   T = length(rets); 
   ht = zeros(T);
@@ -105,24 +105,24 @@ function garchFit(data::Vector)
     end
     sum( log(ht) + (rets./sqrt(ht)).^2 );
   end
-  opt = Opt(:LN_SBPLX,3)
-  lower_bounds!(opt,[1e-10, 0.0, 0.0])
-  upper_bounds!(opt,[1; 0.3; 0.99])
+  opt = Opt(:LN_SBPLX, 3)
+  lower_bounds!(opt, [1e-10, 0.0, 0.0])
+  upper_bounds!(opt, [1; 0.3; 0.99])
   min_objective!(opt, garchLike)
   (minf,minx,ret) = optimize(opt, [1e-5, 0.09, 0.89])
   converged = minx[1]>0 && all(minx[2:3].>=0) && sum(minx[2:3])<1.0
-  H = cdHessian(minx,x->garchLLH(rets,x))
+  H = cdHessian(minx, x->garchLLH(rets, x))
   cvar = -inv(H)
   secoef = sqrt(diag(cvar))
   tval = minx./secoef
-  out = GarchFit(data, minx, -0.5*(T-1)*log(2*pi)-0.5*minf, ret, converged, sqrt(ht),H,cvar,secoef,tval)
+  out = GarchFit(data, minx, -0.5*(T-1)*log(2*pi)-0.5*minf, ret, converged, sqrt(ht), H, cvar, secoef, tval)
 end
 
 function garchPkgTest()
-  println("Running GARCH package test...")
+  info("Running GARCH package test...")
   try
-    include(Pkg.dir("GARCH", "test","GARCHtest.jl"))
-    println("All tests passed!")
+    include(Pkg.dir("GARCH", "test", "runtests.jl"))
+    info("All tests passed!")
   catch err
     throw(err)
   end
