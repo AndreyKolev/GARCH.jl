46f35f65c818e3feb443dfa40b8dae7549146673
ShaoWei Teo <shaowei@macrolighthouse.com>
2015-10-15 14:06:59 +0800

    Use `FactCheck` instead of `Base.Test` to perform testing.
    `FactCheck` gives an informative feedback, which will be useful as a debugging tool going ahead.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,16 @@
 module TestCapsule
 
-using Base.Test
+using FactCheck
 using GARCH
 
-filename = Pkg.dir("GARCH", "test","data","SPY.csv")
-close = map(Float64,readcsv(filename)[:,2])
-ret = diff(log(close))
-ret = ret .- mean(ret)
-fit = garchFit(ret)
-param = [2.469347e-06, 1.142268e-01, 8.691734e-01] #R fGarch garch(1,1) estimated params
-@test_approx_eq_eps(fit.params,param, 1e-3)
+facts("Consistency with R's fGarch") do
+    filename = Pkg.dir("GARCH", "test", "data", "SPY.csv")
+    close = map(Float64, readcsv(filename)[:, 2])
+    ret = diff(log(close))
+    ret = ret .- mean(ret)
+    fit = garchFit(ret)
+    param = [2.469347e-06, 1.142268e-01, 8.691734e-01] #R fGarch garch(1,1) estimated params
+    @fact fit.params --> roughly(param, atol=1e-3)
+end
 
 end #TestCapsule
